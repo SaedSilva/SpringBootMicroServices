19f0cdadbc2552b4766ff081c2b291f75b465816
Saed Sousa <saed.sousa@hotmail.com>
2024-05-27 21:30:45 -0300

    Iniciando Projeto

---
+++ user-service-api/src/main/java/br/dev/saed/userserviceapi/UserServiceApiApplication.java
@@ -0,0 +1,13 @@
+package br.dev.saed.userserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApiApplication.class, args);
+    }
+
+}
---
+++ user-service-api/src/test/java/br/dev/saed/userserviceapi/UserServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.saed.userserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

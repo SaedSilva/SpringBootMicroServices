2c32e736a1d0543b8c76185c08c077cfcf0e9315
Saed Sousa <saed.sousa@hotmail.com>
2024-06-09 19:14:23 -0300

    exception handler for object not found

--- user-service-api/src/main/java/br/dev/saed/userserviceapi/mapper/UserMapper.java
+++ user-service-api/src/main/java/br/dev/saed/userserviceapi/mapper/UserMapper.java
@@ -3,14 +3,11 @@ package br.dev.saed.userserviceapi.mapper;
 import br.dev.saed.userserviceapi.entity.User;
 import models.reponses.UserResponse;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import static org.mapstruct.NullValueCheckStrategy.ALWAYS;
 import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = IGNORE, nullValueCheckStrategy = ALWAYS)
 public interface UserMapper {
-
-
     UserResponse fromEntity(final User entity);
 }
--- user-service-api/src/main/java/br/dev/saed/userserviceapi/service/UserService.java
+++ user-service-api/src/main/java/br/dev/saed/userserviceapi/service/UserService.java
@@ -1,21 +1,20 @@
 package br.dev.saed.userserviceapi.service;
 
-import br.dev.saed.userserviceapi.entity.User;
 import br.dev.saed.userserviceapi.mapper.UserMapper;
 import br.dev.saed.userserviceapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import models.exceptions.ResourceNotFoundException;
 import models.reponses.UserResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-
 public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
     public UserResponse findById(final String id) {
-        return userMapper.fromEntity(userRepository.findById(id).orElse(null));
+        return userMapper.fromEntity(userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Object not found. Id: " + id + ", Type: " + UserResponse.class.getSimpleName())));
 //        return userRepository.findById(id).orElse(null);
     }
 }

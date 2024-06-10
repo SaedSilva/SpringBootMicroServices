6645828c0cabcf22276846d6a7d435d929f7c998
Saed Sousa <saed.sousa@hotmail.com>
2024-06-10 10:51:00 -0300

    add openapi docs

--- user-service-api/src/main/java/br/dev/saed/userserviceapi/controller/exceptions/ControllerExceptionHandler.java
+++ user-service-api/src/main/java/br/dev/saed/userserviceapi/controller/exceptions/ControllerExceptionHandler.java
@@ -3,11 +3,9 @@ package br.dev.saed.userserviceapi.controller.exceptions;
 import jakarta.servlet.http.HttpServletRequest;
 import models.exceptions.ResourceNotFoundException;
 import models.exceptions.StandardError;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.ResourceAccessException;
 
 import java.time.LocalDateTime;
 
